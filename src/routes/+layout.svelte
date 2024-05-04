71ed12f37dbf816cb14c4a04ed90a3a59028eae3
Marco Moroni <marcomoroni394@gmail.com>
2024-05-04 12:18:11 +0200

    reset.css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../styles/reset.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>Via Virtuosa</title>
+</svelte:head>
+
+{@render children()}
